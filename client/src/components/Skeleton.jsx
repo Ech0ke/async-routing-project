e1d57f3bdfe76ff9e81e364e5cfc6b198fa9a618
Ech0ke <aidas.zygas@gmail.com>
2023-09-27 14:03:59 +0300

    Added skeleton loading for User page

--- client/src/components/Skeleton.jsx
+++ client/src/components/Skeleton.jsx
@@ -1,14 +1,15 @@
-import { Fragment } from "react";
-import { Link } from "react-router-dom";
+import { Fragment, Suspense } from "react";
+import { Await, Link } from "react-router-dom";
 
 const widthVariants = {
   short: "15em",
   medium: "25em",
 };
 
-export function Skeleton({ width, classNames }) {
+export function Skeleton({ width, inline, classNames }) {
   const skeletonStyles = {
     width: widthVariants[width],
+    display: inline ? "inline-block" : undefined,
   };
   return <div className={`skeleton ${classNames}`} style={skeletonStyles} />;
 }
@@ -61,5 +62,28 @@ export function PostCardSkeleton() {
         <Skeleton classNames={"skeleton-btn"} />
       </div>
     </div>
+  );
+}
+
+export function PostCommentsSkeleton() {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <div className="text-sm mb-1">
+          <Skeleton width={"short"} />
+        </div>
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+      </div>
+    </div>
+  );
+}
+
+export function SimpleSkeletonText({ resolve, children }) {
+  return (
+    <Suspense fallback={<Skeleton width={"short"} inline />}>
+      <Await resolve={resolve}>{children}</Await>
+    </Suspense>
   );
 }
