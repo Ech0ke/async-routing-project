739caec7b14a04d010120d9f5a47aff53811f30c
Ech0ke <aidas.zygas@gmail.com>
2023-09-27 12:47:16 +0300

    Added skeleton loading for UserList

--- client/src/components/Skeleton.jsx
+++ client/src/components/Skeleton.jsx
@@ -1,15 +1,16 @@
 import { Fragment } from "react";
+import { Link } from "react-router-dom";
 
 const widthVariants = {
   short: "15em",
   medium: "25em",
 };
 
-export function Skeleton({ width }) {
+export function Skeleton({ width, classNames }) {
   const skeletonStyles = {
     width: widthVariants[width],
   };
-  return <div className="skeleton" style={skeletonStyles} />;
+  return <div className={`skeleton ${classNames}`} style={skeletonStyles} />;
 }
 
 export function SkeletonList({ amount, children }) {
@@ -20,4 +21,24 @@ export function SkeletonList({ amount, children }) {
       ))}
     </>
   );
+}
+
+export function UsersListSkeleton() {
+  {
+    return (
+      <div className="card">
+        <div className="card-header">
+          <Skeleton width={"short"} />
+        </div>
+        <div className="card-body">
+          <Skeleton width={"short"} />
+          <Skeleton width={"short"} />
+          <Skeleton width={"short"} />
+        </div>
+        <div className="card-footer">
+          <Skeleton classNames={"skeleton-btn"} />
+        </div>
+      </div>
+    );
+  }
 }
--- client/src/pages/UserList.jsx
+++ client/src/pages/UserList.jsx
@@ -1,38 +1,52 @@
-import { Link, useLoaderData } from "react-router-dom"
-import { getUsers } from "../api/users"
+import { Await, Link, defer, useLoaderData } from "react-router-dom";
+import { getUsers } from "../api/users";
+import { Suspense } from "react";
+import { SkeletonList, UsersListSkeleton } from "../components/Skeleton";
 
 function UserList() {
-  const users = useLoaderData()
+  const { usersPromise } = useLoaderData();
 
   return (
     <>
       <h1 className="page-title">Users</h1>
       <div className="card-grid">
-        {users.map(user => (
-          <div key={user.id} className="card">
-            <div className="card-header">{user.name}</div>
-            <div className="card-body">
-              <div>{user.company.name}</div>
-              <div>{user.website}</div>
-              <div>{user.email}</div>
-            </div>
-            <div className="card-footer">
-              <Link className="btn" to={user.id.toString()}>
-                View
-              </Link>
-            </div>
-          </div>
-        ))}
+        <Suspense
+          fallback={
+            <SkeletonList amount={10}>
+              <UsersListSkeleton />
+            </SkeletonList>
+          }
+        >
+          <Await resolve={usersPromise}>
+            {(users) =>
+              users.map((user) => (
+                <div key={user.id} className="card">
+                  <div className="card-header">{user.name}</div>
+                  <div className="card-body">
+                    <div>{user.company.name}</div>
+                    <div>{user.website}</div>
+                    <div>{user.email}</div>
+                  </div>
+                  <div className="card-footer">
+                    <Link className="btn" to={user.id.toString()}>
+                      View
+                    </Link>
+                  </div>
+                </div>
+              ))
+            }
+          </Await>
+        </Suspense>
       </div>
     </>
-  )
+  );
 }
 
 function loader({ request: { signal } }) {
-  return getUsers({ signal })
+  return defer({ usersPromise: getUsers({ signal }) });
 }
 
 export const userListRoute = {
   loader,
   element: <UserList />,
-}
+};
